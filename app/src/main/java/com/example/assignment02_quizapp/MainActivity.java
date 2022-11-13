e6717debef348ea797a4f7f33513f82d6abe9b93
UsmaAslam2610 <usmaaslam2610@gmail.com>
2022-11-13 12:16:42 +0500

    DeclaringObject

--- app/src/main/java/com/example/assignment02_quizapp/MainActivity.java
+++ app/src/main/java/com/example/assignment02_quizapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.assignment02_quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button OptA, OptB, OptC, OptD,next;
+    TextView QNo, Question, Score,CorrectAns, Remarks;
+    String [] statement ={"How many daily prayers are there in Islam?", "What is the holy book called in Islam?",
+            "How many names does Allah have?","Who is the Last Prophet Of Allah?", "How Many Paras of Holy Quran?"};
+    String [] answers = {"5", "Quran", "99", "Hazrat Muhammad(P.B.U.H)", "30"};
+    String [][] options = {{"4", "5", "7","3"}, {"The Holy Quran","Torah","Zabur", "Injil"}, {"99", "100", "101","90" }, {"Hazrat Adam", "Hazrat Muhammad(P.B.U.H)","Hazrat Dawood", "Hazrat Ibraheem"}, {"30","40","10","20"}};
+    ArrayList<Integer> QList = new ArrayList<Integer>(5);
+    ArrayList<Integer> OList = new ArrayList<Integer>(4);
+    int RanQues = 0, index = 0, score = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
