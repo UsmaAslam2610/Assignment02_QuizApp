5bd3c4788d10f6a6f645623bd35b39391fcac179
UsmaAslam2610 <usmaaslam2610@gmail.com>
2023-01-16 12:19:47 +0500

    SelectAllValuesFromDB

--- app/src/main/java/com/example/assignment02_quizapp/DbHelper.java
+++ app/src/main/java/com/example/assignment02_quizapp/DbHelper.java
@@ -16,6 +16,7 @@ public class DbHelper extends SQLiteOpenHelper {
 
     public static final String TABLE_NAME = "Result";
     public static final String COLUMN_ID = "id";
+    public static final String COLUMN_QUESTION = "question";
     public static final String COLUMN_ANSWER = "answer";
     public static final String COLUMN_SUBMITTEDANSWER = "submittedAnswer";
     public static final String COLUMN_STATUS = "status";
@@ -28,6 +29,7 @@ public class DbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( "
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_QUESTION + " TEXT,"
                 + COLUMN_ANSWER + " TEXT,"
                 + COLUMN_SUBMITTEDANSWER + " TEXT,"
                 + COLUMN_STATUS + " TEXT"
@@ -46,6 +48,7 @@ public class DbHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
+        values.put(COLUMN_QUESTION, result.getQuestion());
         values.put(COLUMN_ANSWER, result.getAnswer());
         values.put(COLUMN_SUBMITTEDANSWER, result.getSubmittedAnswer());
         values.put(COLUMN_STATUS, result.getStatus());
@@ -62,14 +65,33 @@ public class DbHelper extends SQLiteOpenHelper {
         db.close();
         return right;
     }
-    public int truncate() {
+    public void truncate() {
         String sql = "DELETE FROM "+ TABLE_NAME + ";";
 
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL(sql);
         db.close();
+    }
+    public List<Result> selectAllStudents() {
+        List<Result> results = new ArrayList<>();
 
-        return 1;
+        String sql = "SELECT * FROM " + TABLE_NAME;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+        if (cursor.moveToFirst()) {
+            do {
+                @SuppressLint("Range") int id = cursor.getInt(cursor.getColumnIndex(COLUMN_ID));
+                @SuppressLint("Range") String question = cursor.getString(cursor.getColumnIndex(COLUMN_QUESTION));
+                @SuppressLint("Range") String answer = cursor.getString(cursor.getColumnIndex(COLUMN_ANSWER));
+                @SuppressLint("Range") String submittedAnswer = cursor.getString(cursor.getColumnIndex(COLUMN_SUBMITTEDANSWER));
+                @SuppressLint("Range") String status = cursor.getString(cursor.getColumnIndex(COLUMN_STATUS));
+                results.add(new Result(question,answer,submittedAnswer,status));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return results;
     }
 }
 
