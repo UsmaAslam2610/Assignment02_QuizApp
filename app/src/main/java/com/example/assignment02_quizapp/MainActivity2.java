a72d24c7cbe38d7d4d0d7fc8f64c762f1de87650
UsmaAslam2610 <usmaaslam2610@gmail.com>
2023-01-16 12:21:30 +0500

    Activity2Created

---
+++ app/src/main/java/com/example/assignment02_quizapp/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.assignment02_quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+//        Uri webpage = Uri.parse("https://github.com/UsmaAslam2610/Assignment02_QuizApp/commits/master");
+//        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+//        startActivity(intent);
+    }
+
+}
