fcc7f5a44858a6dca1ec20111c6769870503d49b
UsmaAslam2610 <usmaaslam2610@gmail.com>
2023-01-16 12:24:48 +0500

    CommitsLink

--- app/src/main/java/com/example/assignment02_quizapp/MainActivity2.java
+++ app/src/main/java/com/example/assignment02_quizapp/MainActivity2.java
@@ -12,9 +12,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-//        Uri webpage = Uri.parse("https://github.com/UsmaAslam2610/Assignment02_QuizApp/commits/master");
-//        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-//        startActivity(intent);
+        Uri webpage = Uri.parse("https://github.com/UsmaAslam2610/Assignment02_QuizApp/commits/master");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
     }
 
 }
